1cfe1b914bd3d5bd031526c70ee33f801d09e760
leonardo <leonardoaugusto1000a@gmail.com>
2024-08-21 09:04:27 -0300

    explorando a classe main

---
+++ Anatomia-das-Classes/src/MinhaClass.java
@@ -0,0 +1,10 @@
+public class MinhaClass {
+
+    // A classe main em Java é uma classe especial que contém o ponto de entrada do programa, ou seja, 
+    // o método que é executado quando você inicia o programa. Esse método é chamado de main e tem uma assinatura específica.
+    public static void main (String[] args){
+        System.out.print ("Print da classe executavel");
+
+        
+    }
+}
