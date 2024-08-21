542b2b31859aa5f0839e0f913b9b071727a724c8
leonardo <leonardoaugusto1000a@gmail.com>
2024-08-21 16:38:53 -0300

    primitivos Constantes e Casting

--- introducao/src/edu/formacaojava/introducao/TiposVariaveis.java
+++ introducao/src/edu/formacaojava/introducao/TiposVariaveis.java
@@ -1,5 +1,19 @@
 package edu.formacaojava.introducao;
 
 public class TiposVariaveis {
+    // Tipos primitivos que não são considerados como objetos
+    
+    // variáveis
+    double salarioMinimo = 2500;
 
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+
+    // Casting é o processo de converter um valor de um tipo de dado para outro. 
+    // Esse processo pode ser necessário quando você precisa tratar variáveis de diferentes tipos de dados,
+    short numeroCurto2 = (short) numeroNormal;
+
+
+    // Constantes por convenção devemos colocar em caixa alta
+    final double VALOR_DE_PI = 3.14159;
 }
