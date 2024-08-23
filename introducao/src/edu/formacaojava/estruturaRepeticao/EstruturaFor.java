f8c97bd82a0c4be6e32cfc77eba029a632ddcae4
Leonardo <leonardoaugusto1000a@gmail.com>
2024-08-23 08:48:13 -0300

    Iniciando estudos em lacos de repeticao

---
+++ introducao/src/edu/formacaojava/estruturaRepeticao/EstruturaFor.java
@@ -0,0 +1,12 @@
+package edu.formacaojava.estruturaRepeticao;
+
+public class EstruturaFor {
+
+    public static void main(String[] args) {
+        
+        for (int contarAteVinte = 1; contarAteVinte <= 20; contarAteVinte ++) {
+            System.out.println("Contando: " + contarAteVinte);
+        }
+    }
+
+}
